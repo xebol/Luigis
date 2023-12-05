aa68eaa9338cf40aa427ff5849347abef25b9cec
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-12-05 12:01:48 -0700

    finish About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,18 @@
-import "./About.scss"
+import "./About.scss";
 
 const About = () => {
-  return(
-   <div className="about-container">
-    <div className="about-main">
-      <h1>About Us</h1>
-      <p>Luigi's restaurant in the Mushroom Kingdom is a delightful dining spot that offers a unique experience to its visitors. The restaurant is the latest venture of Luigi, a beloved figure in the kingdom, known for his skills as a plumber, doctor, ghost hunter, racecar driver, pro tennis player and MUCH more.</p>
+  return (
+    <div className="about-container">
+      <div className="about-main">
+        <h1>About Us</h1>
+        <p>
+          Luigi's restaurant in the Mushroom Kingdom is a delightful dining spot that offers a unique experience to its visitors. The restaurant is the latest venture of Luigi, a beloved figure in the kingdom, known for his skills as a plumber, doctor, ghost hunter, racecar driver, pro tennis player and MUCH more.</p>
 
-      <p>Now, Luigi brings his love for exploration and his passion for quality food to his restaurant. The menu features dishes made with locally-sourced ingredients that highlight the flavors of the Mushroom Kingdom. From classic linguine to innovative creations like Dry Bone's Marrow, the dishes are expertly crafted and bursting with flavors.</p>
+        <p>Now, Luigi brings his love for exploration and his passion for quality food to his restaurant. The menu features dishes made with locally-sourced ingredients that highlight the flavors of the Mushroom Kingdom. From classic linguine to innovative creations like Dry Bone's Marrow, the dishes are expertly crafted and bursting with flavors. </p>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
 
 
