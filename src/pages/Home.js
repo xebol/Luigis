36d8032c0a23334c16d6f8135c718bb5f6ccaa84
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-12-05 09:00:09 -0700

    finish homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import './Home.scss'
+import Luigi from "../images/luigichef-PhotoRoom.png"
 
 const Home = () => {
   return (
     <div className="container">
       <div className="main">
+        <img src={Luigi} alt="luigi-chef" className="chef-logo"/>
         <h1>Welcome to Luigi's!</h1>
       </div>
     </div>
